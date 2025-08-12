9ae84252b1b81c9e7b15bb6f9ee9cf6acf6bd4bb
Snehanshu <120032562+pilot-j@users.noreply.github.com>
2025-08-12 16:51:08 +0530

    Directly inject LoRA adapters

--- gradio_app/app.py
+++ gradio_app/app.py
@@ -1,13 +1,20 @@
-from huggingface_hub import hf_hub_download
+import gdown
 import gradio as gr
 from PIL import Image
 import torch
 from inference.utils.inference_utils import run_controlnet_inference
 from inference.utils.prompt_utils import build_prompt
-from inference.lora.lora_layers import init_lora_attn
+from inference.lora.lora_layers import init_lora_attn, inject_lora_adapter
 from diffusers import DDIMScheduler, UNet2DConditionModel, AutoencoderKL, ControlNetModel
 from transformers import CLIPTextModel, CLIPTokenizer
 import os
+
+
+import requests
+DRIVE_FILE_ID = "1P1TKojDxwfRNkX4qXwefACFjIHYG0k-y"  
+LORA_FILENAME = "lora_adapter_v2.pth"
+
+
 
 # Paths
 pretrained_model_path = "runwayml/stable-diffusion-v1-5"
@@ -19,6 +26,14 @@ weight_dtype = torch.float32
 
 # Load models once
 def load_models():
+
+    # Download LoRA weights if not present
+    if not os.path.exists(LORA_FILENAME):
+        url = f"https://drive.google.com/uc?id={DRIVE_FILE_ID}"
+        print(f"[INFO] Downloading LoRA weights from Google Drive: {url}")
+        gdown.download(url, LORA_FILENAME, quiet=False)
+
+    
     noise_scheduler = DDIMScheduler.from_pretrained(pretrained_model_path, subfolder="scheduler")
     tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_path, subfolder="tokenizer")
     text_encoder = CLIPTextModel.from_pretrained(pretrained_model_path, subfolder="text_encoder").to(device)
@@ -26,6 +41,7 @@ def load_models():
     unet = UNet2DConditionModel.from_pretrained(pretrained_model_path, subfolder="unet").to(device)
     controlnet = ControlNetModel.from_pretrained(controlnet_model_path).to(device)
     init_lora_attn(controlnet, lora_rank = 512)  # Setup LoRA-compatible layers
+    inject_lora_adapter(controlnet, LORA_FILENAME , device)
 
     return {
         "noise_scheduler": noise_scheduler,
@@ -45,10 +61,6 @@ def infer(image, color, structure, material):
     if image is None:
         return None
 
-    repo_id = "pilotj/control_net"
-    filename = "raw_wts_v1_controlnet"
-    wts_path = hf_hub_download(repo_id=repo_id, filename=filename)
-    
     structure = structure.strip() if structure.strip() else "building"
     color = color.strip() if color.strip() else "white"
     material = material.strip() if material.strip() else "concrete"
@@ -64,10 +76,8 @@ def infer(image, color, structure, material):
         vae=models["vae"],
         unet=models["unet"],
         controlnet=models["controlnet"],
-        wts_path=wts_path,
         device=models["device"],
-        weight_dtype=models["weight_dtype"],
-        inject_lora=False
+        weight_dtype=models["weight_dtype"]
     )
 
 # Gradio UI
